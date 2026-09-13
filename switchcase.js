5e4cdcd2b49d336b83cb0eda09dfc434fcfd8788
Sri Pallav <sripallavyerram@gmail.com>
2026-09-13 23:47:51 +0530

    added switchcase.js

---
+++ switchcase.js
@@ -0,0 +1,27 @@
+let day = 2;
+
+switch (day){
+
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wensday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+};
