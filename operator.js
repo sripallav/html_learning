fa5f99b933c888de587c0408ec2f3f1d60284274
Sri Pallav <sripallavyerram@gmail.com>
2026-09-13 23:49:16 +0530

    added operator.js

---
+++ operator.js
@@ -0,0 +1,21 @@
+let a = 10;
+let b = 3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+let x = 10;
+x++;
+console.log(x);
+let y = 20;
+y--;
+console.log(y);
+
+
+
+console.log(10=='10');
+console.log(10==='10');
