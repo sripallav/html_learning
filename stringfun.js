0eb0670e2b1aee916f6800bcbcfc40bd690e3dfb
Sri Pallav <sripallavyerram@gmail.com>
2026-09-14 23:44:57 +0530

    stringfun.js

---
+++ stringfun.js
@@ -0,0 +1,27 @@
+let str = "Welcome to javascript"
+
+console.log(str.toUpperCase());
+
+console.log(str.toLowerCase());
+
+console.log(str.charAt(4));
+
+console.log(str.indexOf('c'));
+
+console.log(str.concat(" classes"));
+
+console.log(str);
+
+console.log(str.slice(2,6));
+
+console.log(str.slice(4));
+
+console.log(str.substring(3,9));
+
+console.log(str.replace("to "," for"));
+
+let fruits="apple, Banana, Mango";
+
+let res=fruits.split(",");
+
+console.log(res);
