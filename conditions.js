64c08a7ad837ab561b4e4f35dff341e58a564a45
Sri Pallav <sripallavyerram@gmail.com>
2026-09-14 23:43:06 +0530

    conditions.js

---
+++ conditions.js
@@ -0,0 +1,35 @@
+let marks = 90;
+
+if(marks>=90){
+
+    console.log("A Grade");
+}
+else if(marks>=70 && marks<90){
+
+    console.log("B grade");
+}
+else if(marks>=45 && marks<75){
+
+    console.log("c grade");
+}
+else{
+    console.log("Fail");
+
+}
+
+
+let num1 = 10;
+let num2 = 60;
+let num3 = 30;
+
+if(num1>num2 && num1>num3){
+    console.log("number 1 is greater");
+}
+else if(num2>num1 && num2>num3){
+    console.log("number2 is greater");
+}
+else{
+    console.log("number3 is greater");
+}
+
+
