c6b5cb27790814df08866bb08850fd636204a70b
Sri Pallav <sripallavyerram@gmail.com>
2026-09-14 23:45:47 +0530

    fuctionwithparameter.js

---
+++ fuctionwithparameter.js
@@ -0,0 +1,31 @@
+// function greetUser(name,age)
+// {
+//     console.log("My Name is "+ name + "and my age is " + age );
+// }
+// greetUser("Siri",10);
+
+
+// function areaCircle(radius)
+// {
+//     let res = 3.142*radius*radius;
+//     console.log("Area of circle = " + res);
+// }
+
+// areaCircle(4.5);
+
+function greetUser(name,age)
+{
+    console.log("My Name is "+ name + "and my age is " + age );
+}
+greetUser("Siri",10);
+greetUser();
+greetUser("sai",39);
+
+
+function areaCircle(radius)
+{
+    let res = 3.142*radius*radius;
+    console.log("Area of circle = " + res);
+}
+
+areaCircle(4.5);
